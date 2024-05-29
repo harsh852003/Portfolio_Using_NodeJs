87a05d90e0d9af30f1f5bdef88bc9067d826474f
Krutik30 <agherakrutik99@gmail.com>
2024-05-29 17:38:50 +0530

    final changes

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -46,7 +46,7 @@ function EmailSection() {
     <>
       <section className='grid md:grid-cols-2 my-12 md:my-12 py-24' id='email'>
         <div>
-          <h5 className='text-2xl font-bold text-orange-600'>Let&apos;s Meet</h5>
+          <h5 className='text-2xl font-bold text-orange-600'>Lets Meet</h5>
           <p className='text-gray-700 text-lg mb-4 max-w-md dark:text-orange-50'>
             Namaste, I am currently learning new things in web development. I am looking for new opportunities, my inbox is always open whether you have a question or just want to say Hi.
           </p>
@@ -73,7 +73,7 @@ function EmailSection() {
             </div>
             <div>
               <label htmlFor='message' className='text-orange-500 block text-md font-bold md:text-xl'>Message</label>
-              <textarea name='message' id='message' className='bg-orange-100 border border-orange-400 placeholder-orange-300 text-orange-500 rounded-lg block w-full p-2' placeholder="Let&apos;s talk about"></textarea>
+              <textarea name='message' id='message' className='bg-orange-100 border border-orange-400 placeholder-orange-300 text-orange-500 rounded-lg block w-full p-2' placeholder="talk about"></textarea>
             </div>
 
             <button type='submit' className='bg-orange-400 hover:bg-orange-500 rounded-lg block w-full p-2 md:p-2.5 md:text-xl'>
